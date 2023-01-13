b11524e1f13563e156ecdd9be573ab6366914801
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2023-01-13 14:19:54 +0100

    needed to rearrange order of which test to run first

--- src/test/function.test.js
+++
@@ -1,6 +0,0 @@
-import { getNumber } from "./function.js";
-
-describe("It returns true if numbers added is higher than 10", () => {
-  expect(getNumber).toBeGreaterThan(10);
-  expect(getNumber).toEqual(number);
-});
